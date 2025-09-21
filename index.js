d802c5f1317460a2993f111b31997b58021e4084
priyapeo <priyapeo@gmail.com>
2025-09-21 18:27:06 +0600

    imange upload added

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ let students = [];
 let idCounter = 1;
 
 const server = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
+  // const parsedUrl = url.parse(req.url, true);
   const method = req.method;
-  const path = parsedUrl.pathname;
+  const path = req.url;
 
   res.setHeader("Content-Type", "application/json");
 
@@ -25,47 +25,47 @@ const server = http.createServer((req, res) => {
   };
 
   if (path === "/students" && method === "GET") {
-    res.getHead(200);
+    res.writeHead(200);
     res.end(JSON.stringify(students));
   } else if (path === "/students" && method === "POST") {
     getRequestBody((err, data) => {
       if (err || !data.name) {
-        res.getHead(400);
+        res.writeHead(400);
         return res.end(JSON.stringify({ message: "Name is required" }));
       }
       const student = { id: idCounter++, name: data.name };
       students.push(student);
-      res.getHead(201);
+      res.writeHead(201);
       res.end(JSON.stringify(student));
     });
   } else if (path.startsWith("/students/") && method === "PUT") {
     const id = parseInt(path.split("/")[2]);
     getRequestBody((err, data) => {
       if (err || !data.name) {
-        res.getHead(400);
+        res.writeHead(400);
         return res.end(JSON.stringify({ message: "Name is required" }));
       }
       const student = students.find((s) => s.id === id);
       if (!student) {
-        res.getHead(404);
+        res.writeHead(404);
         return res.end(JSON.stringify({ message: "Student not found" }));
       }
       student.name = data.name;
-      res.getHead(200);
+      res.writeHead(200);
       res.end(JSON.stringify(student));
     });
   } else if (path.startsWith("/students/") && method === "DELETE") {
     const id = parseInt(path.split("/")[2]);
-    const index = students.findIndex((s) => s.id === id);
-    if (index === -1) {
-      res.getHead(404);
-      return res.end(JSON.stringify({ message: "Student not found" }));
-    }
-    students.splice(index, 1);
-    res.getHead(200);
+      const student = students.find((s) => s.id === id);
+      if (!student) {
+        res.writeHead(404);
+        return res.end(JSON.stringify({ message: "Student not found" }));
+      }
+    students.splice(student, 1);
+    res.writeHead(200);
     res.end(JSON.stringify({ message: `Student ID ${id} deleted` }));
   } else {
-    res.getHead(404);
+    res.writeHead(404);
     res.end(JSON.stringify({ message: "Route not found" }));
   }
 });
